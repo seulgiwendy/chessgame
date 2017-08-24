79036b7bd4b89315695dbef56b8c42085337f4b3
1 <1@1-PC>
2017-08-24 21:05:43 +0900

    흰색 비숍 Test case 구현

--- src/com/wheejuni/chess/test/PieceTest.java
+++ src/com/wheejuni/chess/test/PieceTest.java
@@ -7,46 +7,59 @@ import org.junit.Test;
 import com.wheejuni.chess.domain.PieceBuilder;
 import com.wheejuni.chess.domain.piecesfactory.BlackPawnCreator;
 import com.wheejuni.chess.domain.piecesfactory.PieceTypeFactory;
+import com.wheejuni.chess.domain.piecesfactory.WhiteBishopCreator;
 import com.wheejuni.chess.pieces.Piece;
 import com.wheejuni.chess.pieces.Piece.Color;
 import com.wheejuni.chess.pieces.Piece.Type;
 
 public class PieceTest {
-	
+
 	PieceTypeFactory ptf;
 
 	@Test
-    public void create_기본생성자() throws Exception {
-        Piece pawn = new Piece();
-        assertEquals(Piece.Color.WHITE.getColor(), pawn.getColor());
-        assertEquals(Piece.Type.PAWN_WHITE.getRepresentation(), pawn.getRepresentation());
-    }
+	public void create_기본생성자() throws Exception {
+		Piece pawn = new Piece();
+		assertEquals(Piece.Color.WHITE.getColor(), pawn.getColor());
+		assertEquals(Piece.Type.PAWN_WHITE.getRepresentation(), pawn.getRepresentation());
+	}
 
-    @Test
-    public void create() {
-        verifyPawn(Piece.Color.WHITE, Piece.Type.PAWN_WHITE);
-        verifyPawn(Piece.Color.BLACK, Piece.Type.PAWN_BLACK);
-    }
-    
-    @Test
-    public void createByInterface() {
-    	Piece piece = newBlackPawn();
-    	System.out.println(piece.getColor());
-    	assertEquals(piece.getColor(), Piece.Color.BLACK.getColor());
-    }
-    
-    
+	@Test
+	public void create() {
+		verifyPawn(Piece.Color.WHITE, Piece.Type.PAWN_WHITE);
+		verifyPawn(Piece.Color.BLACK, Piece.Type.PAWN_BLACK);
+	}
 
-    void verifyPawn(Color color, Type type) {
-        Piece pawn = new Piece(color, type);
-        assertEquals(color.getColor(), pawn.getColor());
-        assertEquals(type.getRepresentation(), pawn.getRepresentation());
-    }
-    
-    Piece newBlackPawn() {
-    	PieceBuilder pb = new PieceBuilder();
-    	pb.setPieceType(new BlackPawnCreator());
-    	return pb.buildPiece();
-    }
+	@Test
+	public void createByInterface() {
+		Piece piece = newBlackPawn();
+		System.out.println(piece.getColor());
+		assertEquals(piece.getColor(), Piece.Color.BLACK.getColor());
+	}
+
+	@Test
+	public void createByInterfaceWhiteBishop() {
+		Piece piece = newWhiteBishop();
+		assertEquals(piece.getRepresentation(), Piece.Type.BISHOP.getRepresentation());
+
+	}
+
+	void verifyPawn(Color color, Type type) {
+		Piece pawn = new Piece(color, type);
+		assertEquals(color.getColor(), pawn.getColor());
+		assertEquals(type.getRepresentation(), pawn.getRepresentation());
+	}
+
+	Piece newBlackPawn() {
+		PieceBuilder pb = new PieceBuilder();
+		pb.setPieceType(new BlackPawnCreator());
+		return pb.buildPiece();
+	}
+
+	Piece newWhiteBishop() {
+		PieceBuilder pb = new PieceBuilder();
+		pb.setPieceType(new WhiteBishopCreator());
+		return pb.buildPiece();
+
+	}
 
 }
